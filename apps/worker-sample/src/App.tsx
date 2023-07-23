2be9d7f7d0a4b443c71656400f2bdcc4fe500376
leejss <goldemshine@gmail.com>
2023-07-23 23:37:26 +0900

    worker-sample:WIP

--- apps/worker-sample/src/App.tsx
+++ apps/worker-sample/src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [number, setNumber] = useState(0);
+  const [primes, setPrimes] = useState<number[]>([]);
+
+  function generatePrimes() {
+    if (!number) return;
+
+    function isPrime(n: number) {
+      for (let c = 2; c <= Math.sqrt(n); ++c) {
+        if (n % c === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    const primes = [];
+    const maximum = 1000000;
+
+    while (primes.length < number) {
+      const candidate = Math.floor(Math.random() * (maximum + 1));
+      if (isPrime(candidate)) {
+        primes.push(candidate);
+      }
+    }
+
+    setPrimes(primes);
+    return primes;
+  }
+
+  useEffect(() => {
+    const worker = new Worker("/worker.js");
+    console.log(worker);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => {
+          const n = Number(e.target.value);
+          if (isNaN(n)) return;
+          setNumber(n);
+        }}
+      />
+      <button onClick={generatePrimes}>get prime</button>
+      {primes.length > 0 && primes.map((n) => <div key={n}>{n}</div>)}
+    </div>
+  );
+};
 
-export default App
+export default App;
