fd1815e5a742ed545e7d02a722052ee67fe1750f
leejss <goldemshine@gmail.com>
2024-04-16 23:55:21 +0900

    add GlowButton

---
+++ apps/make-react-ui/src/components/GlowButton.tsx
@@ -0,0 +1,9 @@
+const GlowButton = () => {
+  return (
+    <button className="bg-[#2c3e50] min-w-[150px] text-white px-[10px] py-[20px] rounded-[5px] transition hover:animate-glow">
+      Glow
+    </button>
+  );
+};
+
+export default GlowButton;
